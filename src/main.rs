a0439b34dbc5b400b701339a30508708b8ce559e
nadmax <maximilien.nadji@pm.me>
2026-04-18 21:12:19 +0200

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,10 @@
+use axum::{Router, routing::get};
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new().route("/", get(|| async { "Hello, World!" }));
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:8080").await.unwrap();
+
+    println!("Listening on http://localhost:8080");
+    axum::serve(listener, app).await.unwrap();
+}
